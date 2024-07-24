a4fb489aaeed48d322cf3cc3a6d21b3b7fb66c69
emerss001 <emersonnevess80@gmail.com>
2024-07-23 22:27:52 -0300

    add setup shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image"
+import { Button } from "./_components/ui/button"
 
 export default function Home() {
-  return <h1 className="mb-5 bg-red-600 text-blue-600">helooo</h1>
+  return (
+    <div>
+      <h1 className="mb-5 bg-red-600 text-blue-600">helooo</h1>
+      <Button>Teste</Button>
+    </div>
+  )
 }
