3a64422fd6f2a663c5fb210e62a40bb038d996d8
emerss001 <emersonnevess80@gmail.com>
2024-08-15 20:41:29 -0300

    feat: ensure only authenticated user can book

--- app/_actions/create-booking.ts
+++ app/_actions/create-booking.ts
@@ -3,14 +3,22 @@
 import { revalidatePath } from "next/cache"
 import { db } from "../_lib/prisma"
 import { endOfDay, startOfDay } from "date-fns"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../_lib/auth"
 
 interface createBookingsParams {
-  userId: string
   serviceId: string
   date: Date
 }
 
 export const createBooking = async (params: createBookingsParams) => {
+  const user = await getServerSession(authOptions)
+  if (!user) {
+    throw new Error("Usuário não autenticado!")
+  }
+
+  const userId = (user.user as any).id
+
   const bookings = await db.booking.findMany({
     where: {
       AND: [
@@ -18,7 +26,7 @@ export const createBooking = async (params: createBookingsParams) => {
           serviceId: params.serviceId,
         },
         {
-          userId: params.userId,
+          userId: userId,
         },
         {
           date: {
@@ -38,7 +46,7 @@ export const createBooking = async (params: createBookingsParams) => {
   }
 
   await db.booking.create({
-    data: params,
+    data: { ...params, userId: userId },
   })
 
   revalidatePath("barbershops/[id]")
