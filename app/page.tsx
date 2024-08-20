7751089e538800e8fdf836c4fd55823a5997fc3f
emerss001 <emersonnevess80@gmail.com>
2024-08-20 19:17:07 -0300

    feat: ensure bookings are listed on bookings page

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,11 @@ import BarbershopItem from "./_components/barbershop-item"
 import { quickSearchOptions } from "./_constants/search"
 import BookingItem from "./_components/booking-item"
 import Search from "./_components/search"
+import { getServerSession } from "next-auth"
+import { authOptions } from "./_lib/auth"
 
 export default async function Home() {
+  const session = await getServerSession(authOptions)
   const barbershops = await db.barbershop.findMany({})
   const popularBarbershops = await db.barbershop.findMany({
     orderBy: {
@@ -15,11 +18,34 @@ export default async function Home() {
     },
   })
 
+  const confirmedBookings = session?.user
+    ? await db.booking.findMany({
+        where: {
+          userId: (session.user as any).id,
+          date: {
+            gte: new Date(),
+          },
+        },
+        include: {
+          service: {
+            include: {
+              barbershop: true,
+            },
+          },
+        },
+        orderBy: {
+          date: "asc",
+        },
+      })
+    : []
+
   return (
     <div>
       <Header />
       <div className="p-5">
-        <h2 className="text-xl font-semibold">Olá, Emerson!</h2>
+        <h2 className="text-xl font-semibold">
+          Olá, {session?.user ? session.user.name + "!" : "visitante!"}
+        </h2>
         <p>Segunda, 05 de Agosto</p>
 
         <div className="mt-6">
@@ -49,7 +75,15 @@ export default async function Home() {
           />
         </div>
 
-        <BookingItem />
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+
+        <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {confirmedBookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
