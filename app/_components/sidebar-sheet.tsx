a35abb337909a83e2437ff9da20e048e113b5830
emerss001 <emersonnevess80@gmail.com>
2025-02-03 21:42:43 -0300

    feat: garantindo que botão de agendamentos apareça apenas para usuários logados

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -60,12 +60,14 @@ const SidebarSheet = () => {
           </Button>
         </SheetClose>
 
-        <Button className="justify-start gap-2" variant="ghost" asChild>
-          <Link href="/bookings">
-            <CalendarIcon size={18} />
-            Agendamentos
-          </Link>
-        </Button>
+        {data?.user && (
+          <Button className="justify-start gap-2" variant="ghost" asChild>
+            <Link href="/bookings">
+              <CalendarIcon size={18} />
+              Agendamentos
+            </Link>
+          </Button>
+        )}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
